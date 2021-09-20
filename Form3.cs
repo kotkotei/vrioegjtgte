44ef3ea279ea5aba7be8a0172cf727439450afea
kotkotei <kotkotei@yandex.ru>
2021-09-20 11:21:48 +0700

    ntregoetjio

---
+++ Form3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string pas = "3454";
+            if (textBox1.Text == pas)
+                MessageBox.Show("Молодец");
+
+            else
+            {
+                if (textBox1.Text != pas)
+                    MessageBox.Show("попобуй снова");
+            }
+            
+
+
+        }
+    }
+}
